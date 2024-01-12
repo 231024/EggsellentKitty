ed5ddabd99236f5d23052190f5e1d75d9a80a5cc
Ekaterina Starokozheva <e.starokozheva@pixonic.com>
2024-01-12 22:21:23 +0300

    Fix ValueItem NRE

--- Assets/Scripts/ValueItem.cs
+++ Assets/Scripts/ValueItem.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 
 public class ValueItem : MonoBehaviour
 {
-	private TMP_Text _text;
-
-	private void Awake() => _text = GetComponent<TMP_Text>();
+	[SerializeField] private TMP_Text text;
 
 	private const string ValuePattern = "x{0}";
 	private const string ScorePattern = "Score: {0}";
 
-	public void SetValue(int value) => _text.SetText(string.Format(ValuePattern, value));
+	public void SetValue(int value) => text.SetText(string.Format(ValuePattern, value));
 	
-	public void SetScore(int score) => _text.SetText(string.Format(ScorePattern, score));
+	public void SetScore(int score) => text.SetText(string.Format(ScorePattern, score));
 }
